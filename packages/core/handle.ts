87c93979914ce578130dd42bc7385768891c4d50
Subwaytime <leon.l@nophase.de>
2026-04-30 23:36:30 +0200

    simplify handle

--- packages/core/handle.ts
+++ packages/core/handle.ts
@@ -1,18 +1,11 @@
-import { transformNode } from "./compiler";
 import { generate } from "./generate";
 import { parse } from "./parse";
-import type { TransformedNode } from "./types";
+import type { Node } from "./types";
 
 export function handle(content: string) {
     const ast = parse(content);
-    const transformed = ast
-        .flatMap((n) => {
-            const res = transformNode(n);
-            return Array.isArray(res) ? res : [res];
-        })
-        .filter(Boolean) as TransformedNode[];
 
-    const { code, mappings } = generate(transformed);
+    const { code, mappings } = generate(ast as any);
 
     return { code, mappings };
 }
