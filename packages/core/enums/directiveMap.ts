ad080c1decd3d23184f1a38793b0584a1570172a
Subwaytime <leon.l@nophase.de>
2026-05-01 14:00:40 +0200

    add boolean based directives to directiveMap

--- packages/core/enums/directiveMap.ts
+++ packages/core/enums/directiveMap.ts
@@ -4,12 +4,12 @@ export const DIRECTIVE_MAP_ENUM = {
     'else-if': { directive: 'v-else-if', extras: [] },
     for: { directive: 'v-for', extras: ['key', ':key', 'memo', ':memo'] },
     show: { directive: 'v-show', extras: [] },
-    cloak: { directive: 'v-cloak', extras: [] },
+    cloak: { directive: 'v-cloak', extras: [], boolean: true },
     text: { directive: 'v-text', extras: [] },
     html: { directive: 'v-html', extras: [] },
-    once: { directive: 'v-once', extras: [] },
+    once: { directive: 'v-once', extras: [], boolean: true },
     memo: { directive: 'v-memo', extras: [] },
-    pre: { directive: 'v-pre', extras: [] },
+    pre: { directive: 'v-pre', extras: [], boolean: true },
 } as const;
 
 Object.freeze(DIRECTIVE_MAP_ENUM);
